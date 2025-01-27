3bbd9cb8ef1c876eeaa7582499916f56b97b9669
fritos <68081184+goohieCrusher@users.noreply.github.com>
2025-01-26 20:26:01 -0800

    working

--- src/test/java/com/bao/bank/AccountTest.java
+++ src/test/java/com/bao/bank/AccountTest.java
@@ -52,27 +52,27 @@ public class AccountTest {
     assertEquals(50.0, account.getBalance(), 0.01);
   }
 
-  // @Test
-  // public void testWithdrawInsufficientBalance() {
-  //   Account account = new Account(1, Account.AccountType.PERSONAL, "John Doe", "123 Main St", "555-1234");
-  //   Asset asset = new Asset("Cash", 100.0);
-  //   account.deposit(asset);
-  //   Exception exception = assertThrows(Error.class, () -> {
-  //     account.withdraw(new Asset("Cash", 150.0));
-  //   });
-  //   String expectedMessage = "Insufficient balance of Cash, amount to withdraw $150.00, current balance is $100.00";
-  //   String actualMessage = exception.getMessage();
-  //   assertTrue(actualMessage.contains(expectedMessage));
-  // }
+  @Test
+  public void testWithdrawInsufficientBalance() {
+    Account account = new Account(1, Account.AccountType.PERSONAL, "John Doe", "123 Main St", "555-1234");
+    Asset asset = new Cash(100.0);
+    account.deposit(asset);
+    Error error = assertThrows(Error.class, () -> {
+      account.withdraw(new Cash(150.0));
+    });
+    String expectedMessage = "Insufficient balance of CASH, amount to withdraw $150.00, current balance is $100.00";
+    String actualMessage = error.getMessage();
+    assertEquals(expectedMessage, actualMessage);
+  }
 
-  // @Test
-  // public void testRemoveAllAsset() {
-  //   Account account = new Account(1, Account.AccountType.PERSONAL, "John Doe", "123 Main St", "555-1234");
-  //   Asset asset1 = new Asset("Cash", 100.0);
-  //   Asset asset2 = new Asset("Stocks", 200.0);
-  //   account.deposit(asset1);
-  //   account.deposit(asset2);
-  //   account.removeAllAsset();
-  //   assertEquals(0, account.getAssets().size());
-  // }
+  @Test
+  public void testRemoveAllAsset() {
+    Account account = new Account(1, Account.AccountType.PERSONAL, "John Doe", "123 Main St", "555-1234");
+    Asset asset1 = new Cash( 100.0);
+    Asset asset2 = new Stock("GOOG", 12, 200.0);
+    account.deposit(asset1);
+    account.deposit(asset2);
+    account.removeAllAsset();
+    assertEquals(0, account.getAssets().size());
+  }
 }
